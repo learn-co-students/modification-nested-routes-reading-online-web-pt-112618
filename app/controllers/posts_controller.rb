37ad2b4d6be9eebffda0099dc555d732f777603d
Whitny Edwards <wne4e2@gmail.com>
2019-05-17 21:02:12 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,8 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
-  end
+  @post = Post.new(author_id: params[:author_id])
+end
 
   def create
     @post = Post.new(post_params)
@@ -39,6 +39,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
